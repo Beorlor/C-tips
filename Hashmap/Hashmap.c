c5e59608d2e7205a8dd1dcf93f1500b50a11d6c5
Beorlor <jbdurand2000@gmail.com>
2023-12-22 05:40:40 +0100

    Hashmap start

---
+++ Hashmap/Hashmap.c
@@ -0,0 +1,6 @@
+//We can deal with collision with chain Hashing or linear probing or quadratic probing or double hashing
+//In C++ (std::unordered_map and std::unordered_set) use separate chaining, load factor, rehashing
+
+#include <stdio.h>
+#include <stdlib.h>
+
